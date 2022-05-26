f82f1d18199a6f7572267c0397ab30e49fdb5131
Max H <78988911+MaxNaudek@users.noreply.github.com>
2022-05-25 22:55:12 -0500

    Cambio de nombres de variables y clases en JS, CSS, HTML. Limpieza gral.

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const titulo = document.querySelector(".navbar")
 const subtitulo = document.querySelector(".properties")
 const boton = document.querySelector(".button");
 const footer = document.querySelector(".footer");
-const stars = document.querySelector(".stars");
-const stars1 = document.querySelector(".stars1");
-const stars2 = document.querySelector(".stars2");
-const stars3 = document.querySelector(".stars3");
-const stars4 = document.querySelector(".stars4");
+const figure = document.querySelector(".figure");
+const figure1 = document.querySelector(".figure1");
+const figure2 = document.querySelector(".figure2");
+const figure3 = document.querySelector(".figure3");
+const figure4 = document.querySelector(".figure4");
 const hexProperties = document.querySelector(".hex-properties");
 const hexNavbar = document.querySelector(".hex-navbar")
 const hexFooter = document.querySelector(".hex-footer")
-// const propNavbar = document.querySelector(".Prop-navbar");
-console.log(fondo)
-// titulo.innerHTML = "hola"
-// titulo.style.background = "blue"
-// titulo.style.color = "white"
 
 const colorArray = ["#9EB4FA", "#FABD19", "#FABD19", "#32FA4E", "#FAE8B6", "#9B84FA", "#37FAA7", "#FA6A50", "#FAE855", "#D2F8BE"]
 
@@ -51,33 +46,34 @@ footer.addEventListener("click", function () {
     hexFooter.textContent = colorArray[randomNumberFooter]
 });
 
-stars.addEventListener("click", function () {
+figure.addEventListener("click", function () {
     event.stopPropagation()
-    const randomNumberStars = getRandomNumber();
-    stars.style.backgroundColor = colorArray[randomNumberStars];
+    const randomNumberFigure = getRandomNumber();
+    figure.style.backgroundColor = colorArray[randomNumberFigure];
 });
 
-stars1.addEventListener("click", function () {
+figure1.addEventListener("click", function () {
     event.stopPropagation()
-    const randomNumberStars1 = getRandomNumber();
-    stars1.style.backgroundColor = colorArray[randomNumberStars1];
+    const randomNumberFigure1 = getRandomNumber();
+    figure1.style.backgroundColor = colorArray[randomNumberFigure1];
 });
 
-stars2.addEventListener("click", function () {
+figure2.addEventListener("click", function () {
     event.stopPropagation()
-    const randomNumberStars2 = getRandomNumber();
-    stars2.style.backgroundColor = colorArray[randomNumberStars2];
-});
-stars3.addEventListener("click", function () {
-    event.stopPropagation()
-    const randomNumberStars3 = getRandomNumber();
-    stars3.style.backgroundColor = colorArray[randomNumberStars3];
+    const randomNumberFigure2 = getRandomNumber();
+    figure2.style.backgroundColor = colorArray[randomNumberFigure2];
 });
 
-stars4.addEventListener("click", function () {
+figure3.addEventListener("click", function () {
     event.stopPropagation()
-    const randomNumberStars4 = getRandomNumber();
-    stars4.style.backgroundColor = colorArray[randomNumberStars4];
+    const randomNumberFigure3 = getRandomNumber();
+    figure3.style.backgroundColor = colorArray[randomNumberFigure3];
+});
+
+figure4.addEventListener("click", function () {
+    event.stopPropagation()
+    const randomNumberFigure4 = getRandomNumber();
+    figure4.style.backgroundColor = colorArray[randomNumberFigure4];
 });
 
 boton.addEventListener("click", function () {
@@ -86,22 +82,22 @@ boton.addEventListener("click", function () {
     const randomNumberSubtitulo = getRandomNumber();
     const randomNumberBackground = getRandomNumber();
     const randomNumberFooter = getRandomNumber();
-    const randomNumberStars= getRandomNumber();
-    const randomNumberStars1= getRandomNumber();
-    const randomNumberStars2= getRandomNumber();
-    const randomNumberStars3= getRandomNumber();
-    const randomNumberStars4= getRandomNumber();
-    // boton.style.background = randomNumber
+    const randomNumberFigure= getRandomNumber();
+    const randomNumberFigure1= getRandomNumber();
+    const randomNumberFigure2= getRandomNumber();
+    const randomNumberFigure3= getRandomNumber();
+    const randomNumberFigure4= getRandomNumber();
+
     titulo.style.backgroundColor = colorArray[randomNumberTitulo];
     subtitulo.style.backgroundColor = colorArray[randomNumberSubtitulo];
     fondo.style.backgroundColor = colorArray[randomNumberBackground];
     footer.style.backgroundColor = colorArray[randomNumberFooter];
-    stars.style.backgroundColor = colorArray[randomNumberStars];
-    stars1.style.backgroundColor = colorArray[randomNumberStars1];
-    stars2.style.backgroundColor = colorArray[randomNumberStars2];
-    stars3.style.backgroundColor = colorArray[randomNumberStars3];
-    stars4.style.backgroundColor = colorArray[randomNumberStars4];
-    // colorArray.textContent = colorArray[randomNumber];
+    figure.style.backgroundColor = colorArray[randomNumberFigure];
+    figure1.style.backgroundColor = colorArray[randomNumberFigure1];
+    figure2.style.backgroundColor = colorArray[randomNumberFigure2];
+    figure3.style.backgroundColor = colorArray[randomNumberFigure3];
+    figure4.style.backgroundColor = colorArray[randomNumberFigure4];
+
     hexNavbar.textContent = colorArray[randomNumberTitulo];
     hexProperties.textContent = colorArray[randomNumberSubtitulo];
     hexFooter.textContent = colorArray[randomNumberFooter]
